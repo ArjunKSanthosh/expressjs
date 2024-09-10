24285f03b04396d9b2ad60b282ec9f1522345110
ArjunKSanthosh <arjunk80043@gmail.com>
2024-09-10 08:57:41 +0530

    doneeeee

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-
-const bodyParser=require("body-parser");
+const path=require("path")
 const express=require("express");
 const app=express();
 const PORT=3000;
-app.use(bodyParser.urlencoded())
-
-app.post("/",(req,res)=>{
-    console.log(req.body);
-    
-
-    res.status(200).send("Hello World")
+// app.use(express.static("template")) //this step is also used to load the page
+app.use(express.json()) 
+app.get("/",Token,(req,res)=>{
+    // to load the page
+    // res.status(200).sendFile(path.join(__dirname,"template","index.html"))  
+    // console.log(req.body);
+    console.log("home");
 })
+app.get("/about",(req,res)=>{                                                                                                                           
+    // res.status(201).send("About")
+    res.status(200).sendFile(path.join(__dirname,"template","about.html"))  
+})
+app.get("/contact",(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname,"template","contact.html"))
+})
+app.get("*",(req,res)=>{   
+     res.status(200).sendFile(path.join(__dirname,"template","error.html"))  
 
+    // res.status(404).send("Page Not found")
+})
+function Token(req,res,next){
+    console.log("middleware");
+    next();
+}
 app.listen(PORT,()=>{
     console.log(`server started at http://localhost:${PORT}`);
     
